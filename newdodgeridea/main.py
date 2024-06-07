6729b4a94a5d2b04579169069257f9ce7a1bc69f
Tonanyulo3 <134009946+Tonanyulo3@users.noreply.github.com>
2024-06-07 13:22:16 -0300

    Add files via upload

--- newdodgeridea/main.py
+++ newdodgeridea/main.py
@@ -1,5 +1,5 @@
 import pygame as pg
-import sys, random
+import sys, random, time
 from pygame.locals import *
 from pygame.image import load
 from os.path import join
@@ -11,12 +11,41 @@ SIZE = W, H = 800, 600
 SCREEN = pg.display.set_mode(SIZE)
 CLOCK = pg.time.Clock()
 IMGP = "images"
-ats = True
+pg.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
+
+BLACK = pg.Color(0, 0, 0)
+WHITE = pg.Color(255, 255, 255)
+man = load(join(IMGP, "manface.png")).convert_alpha()
+
+font = pg.font.Font(join(IMGP, "Minecraft.ttf"), 50)
+text1 = font.render("RIGHT CLICK TO START", True, WHITE)
+text2 = font.render("LEFT CLICK TO TIMESTOP", True, WHITE)
+score = 0; start = 0
+
+enemies = pg.sprite.Group() #sprite group for enemies
+
+def reset():
+    global ats, ts, fps, gaming, bgc, bgcr, stext, srect, score
+    ats = True; ts = False
+    fps = 250
+    gaming = False
+    bgc = [0, 0, 0]; bgcr = [255, 255, 255]
+    for i in enemies:
+        i.set_dest()
+    pg.mouse.set_pos((W/2, H/2))
+    #score text
+    stext = font.render(str(score), True, WHITE)
+    srect = stext.get_rect()
+    srect.center = (W/2, H/2)
+
+def bgcolor():
+    if not bgc == [255, 255, 255]:
+        bgc[0] += 1; bgc[1] += 1; bgc[2] += 1
+        bgcr[0] -= 1; bgcr[1] -= 1; bgcr[2] -= 1
 
 class player(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        pg.mouse.set_pos((W/2, H/2))
         self.rgb = [255, 0, 0]; self.pos = 1
         self.v1 = True; self.v2 = False
         self.set_image()
@@ -25,7 +54,8 @@ class player(pg.sprite.Sprite):
     def update(self):
         mpos = pg.mouse.get_pos()
         self.rect.center = mpos
-        pg.draw.circle(SCREEN, (0, 0, 0), mpos, 5)
+        self.blit()
+    def blit(self):
         SCREEN.blit(self.image, self.rect)
         self.set_color()
     def set_color(self):
@@ -48,25 +78,17 @@ class player(pg.sprite.Sprite):
         pg.draw.circle(self.image, self.rgb, (12, 12), 12)
         self.mask = pg.mask.from_surface(self.image)
 
-enemies = pg.sprite.Group() #sprite group for enemies
-
 class enemy(pg.sprite.Sprite):
-    def __init__(self, w, h):
+    def __init__(self, width, height):
         pg.sprite.Sprite.__init__(self, enemies)
-        self.w = w
-        self.h = h
-        #self.image = load(join(IMGP, "circle.png")).convert_alpha()
-        self.image = pg.Surface((self.w, self.h), pg.SRCALPHA)
-        self.image.convert_alpha()
-        pg.draw.ellipse(self.image, (0, 0, 255), (0, 0, self.w, self.h))
-        #pg.draw.rect(self.image, (0, 0, 255), pg.Rect(0, 0, self.w, self.h))
-        self.mask = pg.mask.from_surface(self.image)
+        self.w = width
+        self.h = height
+        self.set_image()
         self.rect = self.image.get_rect()
         
-        self.xr = list(range(-self.w, W + 1, 10))
-        self.yr = list(range(-self.h, H + 1, 10))
+        self.xr = list(range(-self.w-10, W + 11, 10)) #10px difference for appearance
+        self.yr = list(range(-self.h-10, H + 11, 10))
         self.set_dest()
-        
     def get_pos(self):
         if random.choice((1, 0)): #left or right
             x = random.choice((self.xr[0], self.xr[-1]))
@@ -74,12 +96,9 @@ class enemy(pg.sprite.Sprite):
         else: #top or bottom
             x = random.choice(self.xr)
             y = random.choice((self.yr[0], self.yr[-1]))
-
         return (x, y)
-    
     def set_dist(self):
         self.dist = (-self.rect.x + self.dest[0], -self.rect.y + self.dest[1])
-    
     def set_dest(self):
         do = 1 #plz python just implement do while loop already 😭😭
         self.dest = self.get_pos()
@@ -94,7 +113,16 @@ class enemy(pg.sprite.Sprite):
         self.measure = abs(abs(self.dist[0]) - abs(self.dist[1]))/min(abs(self.dist[0]), abs(self.dist[1]))
         #x or y movement constant
         self.constd = "x" if abs(self.dist[0]) > abs(self.dist[1]) else "y"
-
+    def set_image(self):
+        color = random.choices((WHITE, random.choices(([255, 255, 0], [255, 0, 255], [0, 255, 255]))[0], random.choices(([255, 0, 0], [0, 255, 0], [0, 0, 255]))[0]), weights=[1, 25, 40])[0]
+        self.image = pg.Surface((self.w, self.h), pg.SRCALPHA)
+        self.image.convert_alpha()
+        pg.draw.ellipse(self.image, color, (0, 0, self.w, self.h))
+        pg.draw.ellipse(self.image, bgc, (0, 0, self.w, self.h), 2)
+        if color == WHITE: #OOF
+            manface = pg.transform.scale(man, (self.w, self.h))
+            self.image.blit(manface, (0, 0, 0, 0))
+        self.mask = pg.mask.from_surface(self.image)
     def shift(self):
         self.set_dist()
         try: self.compare = abs(abs(self.dist[0]) - abs(self.dist[1]))/min(abs(self.dist[0]), abs(self.dist[1]))
@@ -113,51 +141,85 @@ class enemy(pg.sprite.Sprite):
         #reset starting point and destination
         if self.rect.topleft == self.dest:
             self.set_dest()
+            self.set_image()
 
-A1 = enemy(200, 100)
-A2 = enemy(100, 200)
-A3 = enemy(200, 200)
-A4 = enemy(200, 200)
-A5 = enemy(300, 300)
-A6 = enemy(50, 50)
-A7 = enemy(50, 100)
-A8 = enemy(100, 50)
-A9 = enemy(100, 100)
-A10 = enemy(100, 100)
+reset() #game parameters reset
 
-PLAYER = player()
+enemies.add(enemy(350, 350))
+enemies.add(enemy(250, 250))
+enemies.add(enemy(200, 200))
+enemies.add(enemy(150, 200))
+enemies.add(enemy(200, 150))
+enemies.add(enemy(150, 150))
+enemies.add(enemy(125, 125))
+enemies.add(enemy(150, 100))
+enemies.add(enemy(100, 150))
+enemies.add(enemy(100, 100))
+enemies.add(enemy(100, 50))
+enemies.add(enemy(50, 100))
+enemies.add(enemy(50, 50))
+enemies.add(enemy(50, 50))
+enemies.add(enemy(50, 50))
+
+P = player()
 
 allow_ts = pg.USEREVENT + 0
-pg.time.set_timer(allow_ts, 5000)
+timestop = pg.USEREVENT + 1
+change_color = pg.USEREVENT + 2
+pg.time.set_timer(change_color, 235)
 
 while 1:
     keys = pg.key.get_pressed()
+    mp = pg.mouse.get_pressed()
     for event in pg.event.get():
         if event.type == pg.QUIT or keys[K_ESCAPE]:
             pg.quit()
             sys.exit()
+        if event.type == change_color:
+            if gaming:
+                bgcolor()
         if event.type == allow_ts:
             ats = True
-        if keys[K_SPACE]:
+        if event.type == timestop:
+            ts = False
+        if mp[0]:
             if ats:
-                ats = False
-                pg.time.delay(1000)
+                pg.time.set_timer(allow_ts, 5000)
+                pg.time.set_timer(timestop, 1000)
+                ats = False; ts = True
+        if mp[2]:
+            gaming = True
+            score = 0
+            start = pg.time.get_ticks()
 
-    SCREEN.fill((30, 30, 30))
+    SCREEN.fill(bgc)
 
-    for e in enemies:
-        #collide
-        if pg.sprite.collide_mask(PLAYER, e):
-            pass
-            #print("a")
-        #enemy movement
-        e.shift()
+    #text display on game pause
+    if not gaming:
+        SCREEN.blit(text1, pg.Rect(90, 100, 0, 0))
+        SCREEN.blit(text2, pg.Rect(80, 450, 0, 0))
+        SCREEN.blit(stext, srect)
+    else:
+        #time score calculation
+        score = pg.time.get_ticks() - start
+
+    if not ts:
+        P.update()
+        if gaming: #enemies don't move on timestop and game pause
+            for e in enemies:
+                #enemy movement
+                e.shift()
+                #collide
+                if pg.sprite.collide_mask(P, e):
+                    gaming = False
+                    reset()
 
     enemies.draw(SCREEN)
-    PLAYER.update()
+    pg.draw.circle(SCREEN, bgcr, pg.mouse.get_pos(), 8)
+    P.blit()
 
     pg.display.flip()
-    CLOCK.tick(500)
+    CLOCK.tick(fps)
 
 """
 player ball is black and mouse hidden with smaller black point
@@ -169,6 +231,8 @@ king JIJIJIJA
 snake gif animation
 charlie woo meme
 random letters that spin and appear as a group
-
+MADE IN HEAVEN
+LOL logo
+POOP
 ...
 """
--- newdodgeridea/test.py
+++ newdodgeridea/test.py
@@ -1,65 +1,30 @@
-class pos():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+import pygame as pg
+import sys
 
-a = pos(-145, 60)
-b = pos(800, 410)
-c = pos(-a.x+b.x, -a.y+b.y)
+pg.init()
 
-pepe = "x" if abs(c.y) > abs(c.x) else "y"
+screen = pg.display.set_mode((500, 500))
+clock = pg.time.Clock()
 
-si = 0
-no = 0
+rgb = [0, 0, 0]
 
-e = min(abs(c.x), abs(c.y))
-diff = abs(abs(c.x) - abs(c.y))
-try: si = diff/e
-except: pass
+test = pg.Color(0, 0, 0)
 
-while a.x != b.x or a.y != b.y:
-    print(a.x, a.y)
+changec = pg.USEREVENT + 0
+pg.time.set_timer(changec, 100)
 
-    c.x = -a.x+b.x
-    c.y = -a.y+b.y
-    
-    e = min(abs(c.x), abs(c.y))
-    diff = abs(abs(c.x) - abs(c.y))
+while 1:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+        if rgb != [255, 255, 255]:
+            if event.type == changec:
+                rgb[0] += 1
+                rgb[1] += 1
+                rgb[2] += 1
 
-    try: no = diff/e
-    except: pass
+    screen.fill(rgb)
 
-    if pepe == "x":
-        if no <= si:
-            try: a.x += int(c.x/abs(c.x))
-            except: pass
-        a.y += int(c.y/abs(c.y))
-    elif pepe == "y":
-        if no <= si:
-            try: a.y += int(c.y/abs(c.y))
-            except: pass
-        a.x += int(c.x/abs(c.x))
-
-print(a.x, a.y)
-
-
-"""
-pos(-200;0)
-des(800;300)
-distance(1000;300)1101110110
-700/300
-11010110
-1010110
-
-pos(-200;0)
-des(400;500)
-distance(600;500)111110
-100/500
-go 5 stop 1
-
-max - min (abs)
-
-
-
-
-"""
+    pg.display.flip()
+    clock.tick(60)
